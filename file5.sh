16973750838f9ed1fcb528b76307ffb33a9d28cc
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-02 16:06:09 +0530

    username and passwd

---
+++ file5.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "please enter your username:"
+
+read username
+
+echo "username entered is $username"
+
+echo "please enter your password"
+
+read -s password accepted
+
+echo "password is entered for $username"
