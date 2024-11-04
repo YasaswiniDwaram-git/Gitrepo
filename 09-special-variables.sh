36c9dd8e3c56524b2d1236a15d5cfb3c81df5d1b
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-04 14:52:09 +0530

    until 08 file

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -5,3 +5,7 @@ echo "how many variables passed in the script - $#"
 echo "script name is : $0"
 echo "current working directory :$PWD"
 echo "Home directory of current : $HOME"
+echo "5. To know process id of current command $$"
+echo "6. to know PID of last BG command $!"
+sleep 100 &
+echo "$!"
