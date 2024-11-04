64268c336a32721d432f52b5c9f9fd27506c2183
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-04 14:23:57 +0530

    until 08 file

--- 09-special-variables.sh
+++ 09-special-variables.sh
@@ -3,3 +3,5 @@
 echo "all variables to be sent to $@"
 echo "how many variables passed in the script - $#"
 echo "script name is : $0"
+echo "current working directory :$PWD"
+echo "Home directory of current : $HOME"
