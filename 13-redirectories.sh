057b7adf5a511a328b6c257e2f42fc9b4241d7c1
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-05 21:26:53 +0530

    11th file

--- 13-redirectories.sh
+++ 13-redirectories.sh
@@ -8,7 +8,7 @@ Y="\e[33m]"
 LOGS_FOLDER="/var/log/shell-scriptings"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME/$TIME_STAMP.log"
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
 mkdir -p $LOGS_FOLDER
 
 CHECK_ROOT(){
