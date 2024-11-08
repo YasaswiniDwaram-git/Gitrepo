8168e92f910faf75cc579c261dfc765117413654
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-08 17:22:11 +0530

    11th file

--- delete-log-files.sh
+++ delete-log-files.sh
@@ -14,5 +14,6 @@ echo "Files are : $LOG_FILES"
 
 while IFS= read -r line #internal field seperator ,it will not ingnore white space , -r will not ignore spl characters like /@$#%
 do
-    echo "Deleting line :$line"
+    echo "Deleting line :$line" 
+    rm -rf $line
 done <<< $LOG_FILES  #taking input fron $LOG_FILES
