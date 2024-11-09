192d76d8caafca7e31c1236e1a94a3dcd71f689c
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-09 14:03:07 +0530

    11th file

--- backup.sh
+++ backup.sh
@@ -19,3 +19,24 @@ if [ $# -lt 2 ]
 then
     USAGE
 fi
+
+if [ ! -d $SOURCE_DIRECTORY ]
+then
+    echo -e " $R $SOURCE_DIRECTORY is not present $N , please check"
+fi
+
+if [ ! -d $DESTINATION_DIRECTORY ]
+then
+    echo -e " $R $DESTINATION_DIRECTORY is not present $N, please check"
+fi
+
+LOG_FILES=$(find $OURCE_DIRECTORY -name "*.log" -mtime +14) #to list 14 days logs
+
+#check files if exist or not in the directory , if zero , nothing to do else zip them
+
+if [ -n $LOG_FILES]
+then
+    echo "Files are found"
+else
+    echo " No files older than $NUMBER_OF_DAYS"
+fi
