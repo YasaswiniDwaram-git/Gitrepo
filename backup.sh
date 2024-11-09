19db517c3b41a69c1733c15ef12ea3c8be5b516e
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-09 14:29:09 +0530

    11th file

--- backup.sh
+++ backup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+USERID=$(id -u)
 
 R="\e[31m"
 G="\e[32m"
@@ -12,6 +13,16 @@ NUMBER_OF_DAYS=${3:-14} # if given  , the consider $3 , else will take 14 days
 USAGE(){
     echo -e "$R input format should be : $N <source_directory> <destination_directory> <days(optional)>"
 }
+
+CHECK_ROOT(){
+if [ $USERID -ne 0 ]
+then
+    echo "$R please run script with root access $N"
+    exit 1
+fi
+}
+
+CHECK_ROOT
 
 #check if source and destination are there are not
 # we need atleast 2 arg to proceed , so
@@ -35,11 +46,15 @@ LOG_FILES=$(find ${SOURCE_DIRECTORY} -name "*.log" -mtime +14) #to list 14 days 
 #check files if exist or not in the directory , if zero , nothing to do else zip them
 echo "Files present are : $LOG_FILES"
 
-if [ ! -z $LOG_FILES ] # -z if true if file is empty but ! makes it as files is not empty
+if [ ! -z "$LOG_FILES" ] # -z if true if file is empty but ! makes it as files is not empty
 then
     echo "Files are found"
+    dnf install zip -y
+    find . -name "*.log" -mtime +14 | zip apps.zip -@
 else
     echo " No files older than $NUMBER_OF_DAYS"
 fi
+
+
 
 
