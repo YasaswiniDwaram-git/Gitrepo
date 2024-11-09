264f0e12a32672dfa820c795c32f517d5f3bbf6c
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-09 16:09:18 +0530

    11th file

--- 17-disk-usage.sh
+++ 17-disk-usage.sh
@@ -4,5 +4,10 @@ DISK_USAGE=$(df -hT | grep xfs)
 DISK_THRESHOLD=10 #usually it is 75 for real projects
 while IFS= read -r line #internal field seperator ,it will not ingnore white space , -r will not ignore spl characters like /@$#%
 do
-    echo $line
+    USAGE=$(echo $line | grep xfs | awk -F " " '{print $F}' | cut -d "%" -f1)
+    PARTITION=$(echo $line | grep xfs | aws -F " " '{print $F}')
+    if [ $USAGE -ge $DISK_THRESHOLD ]
+    then
+        echo "$PARTITION is more than $DISK_THRESHOLD , current consumption is $USAGE , please check "
+    fi
 done <<< $DISK_USAGE
