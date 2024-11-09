b05f1bbe2092d3cfafb6301a5d6225a21b02d771
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-09 13:27:39 +0530

    11th file

--- delete-log-files.sh
+++ delete-log-files.sh
@@ -12,8 +12,8 @@ fi
 LOG_FILES=$(find $SOURCE_DIRECTORY -name "*.log" -mtime +14)
 echo "Files are : $LOG_FILES"
 
-while IFS= read -r line #internal field seperator ,it will not ingnore white space , -r will not ignore spl characters like /@$#%
+while IFS= read -r file #internal field seperator ,it will not ingnore white space , -r will not ignore spl characters like /@$#%
 do
-    echo "Deleting line :$line" 
-    rm -rf $line
+    echo "Deleting file :$file" 
+    rm -rf $file
 done <<< $LOG_FILES  #taking input fron $LOG_FILES
