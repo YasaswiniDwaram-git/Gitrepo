96e3836fa1dde186b3e12abbf3483a0940d19e05
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-10 11:04:13 +0530

    11th file

--- 18.-count-words.sh
+++ 18.-count-words.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
 READ_FILE=/home/ec2-user/sometext.txt
-WORD_COUNT=$(cat $READ_FILE |wc)
+
 
 while IFS= read -r line #internal field seperator ,it will not ingnore white space , -r will not ignore spl characters like /@$#%
-do
-    echo "total words in line :$WORD_COUNT "
-    FORMAT=$(awk -F '{print $5F}' $READ_FILE )
-done <<< $READ_FILE
+do 
+    WORD_COUNT=$(echo $line |wc -w)
+    echo "Total words in $line is : $WORD_COUNT"
+    echo "$line" | cut -d " "  -f1-5
+done < $READ_FILE
 
