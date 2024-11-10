d31ae66c0c2bfb54b93ae821a99e92bd0316ea07
Yasaswini <yasaswinidwaram07@gmail.com>
2024-11-10 11:06:05 +0530

    11th file

--- 18.-count-words.sh
+++ 18.-count-words.sh
@@ -6,7 +6,7 @@ READ_FILE=/home/ec2-user/sometext.txt
 while IFS= read -r line #internal field seperator ,it will not ingnore white space , -r will not ignore spl characters like /@$#%
 do 
     WORD_COUNT=$(echo $line |wc -w)
-    echo "Total words in $line is : $WORD_COUNT"
-    echo "$line" | cut -d " "  -f1-5
+    echo "Total words in line is : $WORD_COUNT"
+    echo "First 5 word are : $line" | cut -d " "  -f1-5
 done < $READ_FILE
 
